ee49c5b31151ba3852869ec721091adf205bae6a
rawooldipesh <rawooldipesh0@gmail.com>
2025-09-12 08:28:36 +0530

    expo version change

--- utils/config.ts
+++ utils/config.ts
@@ -6,7 +6,7 @@ const USE_LAN = true; // set false if testing on emulator
 export const API_BASE_URL =
   __DEV__
     ? USE_LAN
-      ? "http://192.168.0.101:5000/api"
+      ? "http://192.168.0.102:5000/api"
       : "http://localhost:5000/api"
     : "https://your-backend-url.com";
 
